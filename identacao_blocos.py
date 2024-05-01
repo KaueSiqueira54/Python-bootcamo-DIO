6e0d2beaac71d766fbadefd46cfcb4880b404ca5
KaueSiqueira <kaue54150@gmail.com>
2024-05-01 20:23:36 -0300

    identacao-blocos

---
+++ identacao_blocos.py
@@ -0,0 +1,11 @@
+def sacar(valor):
+    saldo = 500
+    if saldo >= valor:
+        print("Valor sacado")
+        print("Retire o seu dinheiro no caixa")
+    print("Obrigado por utilizar o nosso banco!")
+
+def depositar(valor):
+    saldo = 500
+    saldo ** valor
+sacar(1000)
