80015d605a95fea59e744546a4cf0a258c8b25ad
KaueSiqueira <kaue54150@gmail.com>
2024-05-03 17:29:52 -0300

    estrutura-while

---
+++ estruturas-condicao-repeticao/estrutura-while.py
@@ -0,0 +1,17 @@
+###'''opcao = 1
+
+#while opcao != 0:
+  #  opcao = int(input("[1] Sacar \n[2] Extrato \n[0] Sair \n" ))
+  #  if opcao ==1:
+ #       print("sacando")
+ #   elif opcao == 2:
+#else:
+###    print("Obrigado por utilizar o programa!")'''
+
+numero = -1
+
+while True:
+    numero = int(input("Informe um número>: "))
+    if numero == 10:
+        break
+    print(numero)
