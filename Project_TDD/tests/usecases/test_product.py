f01b91132efe83b86b68da5f39ffd75dd40cbabf
KaueSiqueira <kaue54150@gmail.com>
2024-06-25 21:32:11 -0300

    criação do mongo

---
+++ Project_TDD/tests/usecases/test_product.py
@@ -0,0 +1,9 @@
+from store.usecases.product import product_usecase
+
+
+async def test_usecasses_should_return_sucess(product_in):
+    result = await product_usecase.create(body=product_in)
+
+    #assert isinstance(result, ProductOut)
+    assert result is None
+    
